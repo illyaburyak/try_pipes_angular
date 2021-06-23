68b29db3ab41403eae3e45149699a5612a802238
Illya Buryak <illyabenavente@gmail.com>
2021-06-23 17:53:49 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipesOverview';
+  name: string = '';
+  date: string = '';
+  amount: number = 0;
+  height: number = 0;
+  miles: number = 0;
+
+
+  onMilesChange(value: any) {
+    this.miles = parseFloat(value.target.value);
+  }
+
+
+  onHeightChange(value: any) {
+    this.height = parseFloat(value.target.value);
+  }
+
+
+  onNameChange(value: any) {
+    this.name = value.target.value;
+  }
+
+  onDateChange(value: any) {
+    this.date = value.target.value;
+  }
+
+  onAmountChange(value: any) {
+    this.amount = parseFloat(value.target.value);
+  }
 }
+
+
